b461ca79a66e13e1c55d7a3642a0786a686b4ae1
mika1407 <koneautomaatio@hotmail.com>
2020-03-10 08:02:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Immediately invoked function expression
+// to not pollute the global scope
+(function() {
+  const wheel = document.querySelector(".wheel");
+  const startButton = document.querySelector(".button");
+  let deg = 0;
+})();
